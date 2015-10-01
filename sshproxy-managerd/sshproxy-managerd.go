67acedcd6988fee69857a45b3047fd7e53d2c211
Arnaud Guignard <arnaud.guignard@cea.fr>
2015-10-01 11:48:43 +0200

    add copyright header to source files

--- sshproxy-managerd/sshproxy-managerd.go
+++ sshproxy-managerd/sshproxy-managerd.go
@@ -1,3 +1,12 @@
+// Copyright 2015 CEA/DAM/DIF
+//  Contributor: Arnaud Guignard <arnaud.guignard@cea.fr>
+//
+// This software is governed by the CeCILL-B license under French law and
+// abiding by the rules of distribution of free software.  You can  use,
+// modify and/ or redistribute the software under the terms of the CeCILL-B
+// license as circulated by CEA, CNRS and INRIA at the following URL
+// "http://www.cecill.info".
+
 package main
 
 import (
