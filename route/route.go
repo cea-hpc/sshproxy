cea-hpc/sshproxy

commit f082e38229c2db1789427835415e39d969b525ae

always use hostport
Instead of separated strings host, port.

route/route.go
@@ -15,14 +15,12 @@ import (
 	"net"
 	"time"
 
-	"sshproxy/utils"
-
 	"github.com/op/go-logging"
 )
 
 var log = logging.MustGetLogger("sshproxy/route")
 
-type selectDestinationFunc func([]string, HostChecker) (string, string, error)
+type selectDestinationFunc func([]string, HostChecker) (string, error)
 
 var (
 	// default algorithm to find route
@@ -35,15 +33,15 @@ var (
 //
 // Check tests if a connection to host:port can be made.
 type HostChecker interface {
-	Check(host, port string) bool
+	Check(hostport string) bool
 }
 
 // BasicHostChecker implements the HostChecker interface.
 type BasicHostChecker struct{}
 
 // Check tests if a conneciton to host:port can be made with a 1s timeout.
-func (bhc *BasicHostChecker) Check(host, port string) bool {
-	return CanConnect(host, port)
+func (bhc *BasicHostChecker) Check(hostport string) bool {
+	return CanConnect(hostport)
 }
 
 var (
@@ -54,10 +52,10 @@ var (
 )
 
 // CanConnect tests if a connection to host:port can be made (with a 1s timeout).
-func CanConnect(host, port string) bool {
-	c, err := net.DialTimeout("tcp", net.JoinHostPort(host, port), 1*time.Second)
+func CanConnect(hostport string) bool {
+	c, err := net.DialTimeout("tcp", hostport, 1*time.Second)
 	if err != nil {
-		log.Info("cannot connect to %s: %s", net.JoinHostPort(host, port), err)
+		log.Info("cannot connect to %s: %s", hostport, err)
 		return false
 	}
 	c.Close()
@@ -65,30 +63,25 @@ func CanConnect(host, port string) bool {
 }
 
 // selectDestinationOrdered selects the first reachable destination from a list
-// of destinations. It returns its host and port.
-func selectDestinationOrdered(destinations []string, checker HostChecker) (string, string, error) {
+// of destinations. It returns a string "host:port" or an error.
+func selectDestinationOrdered(destinations []string, checker HostChecker) (string, error) {
 	for i, dst := range destinations {
-		host, port, err := utils.SplitHostPort(dst)
-		if err != nil {
-			return "", "", err
-		}
-
 		// always return the last destination without trying to connect
 		if i == len(destinations)-1 {
-			return host, port, nil
+			return dst, nil
 		}
 
-		if checker == nil || checker.Check(host, port) {
-			return host, port, nil
+		if checker == nil || checker.Check(dst) {
+			return dst, nil
 		}
 	}
-	return "", "", fmt.Errorf("no valid destination found")
+	return "", fmt.Errorf("no valid destination found")
 }
 
 // selectDestinationRandom randomizes the order of the provided list of
 // destinations and selects the first reachable one. It returns its host and
 // port.
-func selectDestinationRandom(destinations []string, checker HostChecker) (string, string, error) {
+func selectDestinationRandom(destinations []string, checker HostChecker) (string, error) {
 	rand.Seed(time.Now().UnixNano())
 	rdestinations := make([]string, len(destinations))
 	perm := rand.Perm(len(destinations))
@@ -99,7 +92,7 @@ func selectDestinationRandom(destinations []string, checker HostChecker) (string
 	return selectDestinationOrdered(rdestinations, checker)
 }
 
-func Select(route_select string, destinations []string, checker HostChecker) (string, string, error) {
+func Select(route_select string, destinations []string, checker HostChecker) (string, error) {
 	return routeSelecters[route_select](destinations, checker)
 }
 

manager/client.go
@@ -97,18 +97,16 @@ func (c *Client) sendCommand(command string) (string, error) {
 }
 
 // Connect sends a connection request to the manager.
-// It returns an IP address with a port or an error.
-func (c *Client) Connect() (string, string, error) {
+// It returns a string "host:port" or an error.
+func (c *Client) Connect() (string, error) {
 	hostport, err := c.sendCommand(fmt.Sprintf("connect %s %s", c.Username, c.Sshd))
 	if err != nil {
-		return "", "", err
+		return "", err
 	}
-	hostport := strings.TrimSpace(line)
-	host, port, err := net.SplitHostPort(hostport)
-	if err != nil {
-		return "", "", fmt.Errorf("invalid response from %s [=%s]: %s", c.Manager, hostport, err)
+	if _, _, err = net.SplitHostPort(hostport); err != nil {
+		return "", fmt.Errorf("invalid response from %s [=%s]: %s", c.Manager, hostport, err)
 	}
-	return host, port, nil
+	return hostport, nil
 }
 
 // Disconnect sends a disconnection request to the manager.

sshproxy-managerd/sshproxy-managerd.go
@@ -142,18 +142,17 @@ func NewHostChecker() *hostChecker {
 // than config.Check_Interval duration it returns its known state. Otherwise it
 // performs a check and updates (or adds a state to) the internal view
 // accordingly.
-func (hc *hostChecker) Check(host, port string) bool {
+func (hc *hostChecker) Check(hostport string) bool {
 	ts := time.Now()
-	dst := net.JoinHostPort(host, port)
 	var state State
-	s, ok := hc.States[dst]
+	s, ok := hc.States[hostport]
 	switch {
 	case !ok:
-		state = hc.DoCheck(host, port)
+		state = hc.DoCheck(hostport)
 	case s.State == Disabled:
 		state = s.State
 	case ts.Sub(s.Ts) > config.Check_Interval.Duration():
-		state = hc.DoCheck(host, port)
+		state = hc.DoCheck(hostport)
 	default:
 		state = s.State
 	}
@@ -161,19 +160,18 @@ func (hc *hostChecker) Check(host, port string) bool {
 }
 
 // DoCheck checks if an host is alive and updates the internal view.
-func (hc *hostChecker) DoCheck(host, port string) State {
+func (hc *hostChecker) DoCheck(hostport string) State {
 	state := Down
-	if route.CanConnect(host, port) {
+	if route.CanConnect(hostport) {
 		state = Up
 	}
-	hc.Update(host, port, state, time.Now())
+	hc.Update(hostport, state, time.Now())
 	return state
 }
 
 // Update updates (or creates) the state of an host in the internal view.
-func (hc *hostChecker) Update(host, port string, state State, ts time.Time) {
-	dst := net.JoinHostPort(host, port)
-	if s, ok := hc.States[dst]; ok {
+func (hc *hostChecker) Update(hostport string, state State, ts time.Time) {
+	if s, ok := hc.States[hostport]; ok {
 		s.State = state
 		s.Ts = ts
 	} else {
@@ -181,13 +179,13 @@ func (hc *hostChecker) Update(host, port string, state State, ts time.Time) {
 			State: state,
 			Ts:    ts,
 		}
-		hc.States[dst] = s
+		hc.States[hostport] = s
 	}
 }
 
 // IsDisabled checks if an host is disabled.
-func (hc *hostChecker) IsDisabled(host, port string) bool {
-	if s, ok := hc.States[net.JoinHostPort(host, port)]; ok {
+func (hc *hostChecker) IsDisabled(hostport string) bool {
+	if s, ok := hc.States[hostport]; ok {
 		return s.State == Disabled
 	}
 	return false
@@ -227,15 +225,12 @@ func getAlgorithmAndRoutes(user, hostport string, groups map[string]bool) (strin
 
 	algo := ""
 	dests := []string{}
-	host, port, _ := utils.SplitHostPort(hostport)
 
 	for _, cfg := range configs {
 		if cfg.Route_Select != "" {
 			algo = cfg.Route_Select
 		}
 		if d, ok := cfg.Routes[hostport]; ok {
-			dests = d
-		} else if d, ok := cfg.Routes[host]; port == utils.DefaultSshPort && ok {
 			dests = d
 		} else if d, ok := cfg.Routes[route.DefaultRouteKeyword]; ok {
 			dests = d
@@ -256,12 +251,20 @@ func selectRoute(user, hostport string) (string, error) {
 
 	algo, dests := getAlgorithmAndRoutes(user, hostport, groups)
 
-	dhost, dport, err := route.Select(algo, dests, managerHostChecker)
+	dst, err := route.Select(algo, dests, managerHostChecker)
 	if err != nil {
 		return "", fmt.Errorf("cannot select route for user '%s': %s", user, err)
 	}
 
-	return net.JoinHostPort(dhost, dport), nil
+	return dst, nil
+}
+
+func checkHostPort(hostport string) (string, error) {
+	host, port, err := utils.SplitHostPort(hostport)
+	if err != nil {
+		return "", err
+	}
+	return net.JoinHostPort(host, port), nil
 }
 
 // commandHandler represents an handler for a protocol command.
@@ -298,8 +301,9 @@ func connectHandler(args []string) (string, error) {
 		return "", notEnoughArgumentsError
 	}
 
-	user, hostport := args[0], args[1]
-	if _, _, err := utils.SplitHostPort(hostport); err != nil {
+	user := args[0]
+	hostport, err := checkHostPort(args[1])
+	if err != nil {
 		return "", invalidHostError
 	}
 
@@ -307,15 +311,14 @@ func connectHandler(args []string) (string, error) {
 	pc, ok := proxiedConnections[key]
 	if ok {
 		log.Info("found connection for %s: %s", key, pc.Dest)
-		host, port, _ := utils.SplitHostPort(pc.Dest)
-		if managerHostChecker.Check(host, port) {
+		if managerHostChecker.Check(pc.Dest) {
 			pc.N++
 			pc.Ts = time.Now()
 			return fmt.Sprintf("+%s", pc.Dest), nil
 		} else {
 			log.Info("cannot connect %s to already existing connection(s) to %s: host down or disabled", key, pc.Dest)
-			if !managerHostChecker.IsDisabled(host, port) {
-				managerHostChecker.Update(host, port, Down, time.Now())
+			if !managerHostChecker.IsDisabled(pc.Dest) {
+				managerHostChecker.Update(pc.Dest, Down, time.Now())
 			}
 		}
 	}
@@ -335,7 +338,7 @@ func connectHandler(args []string) (string, error) {
 	return fmt.Sprintf("+%s", dst), nil
 }
 
-// disableHandler handles the "disable host:port command.
+// disableHandler handles the "disable host[:port] command.
 //
 // It returns "+OK" or an error message.
 func disableHandler(args []string) (string, error) {
@@ -343,12 +346,12 @@ func disableHandler(args []string) (string, error) {
 		return "", notEnoughArgumentsError
 	}
 
-	hostport := args[0]
-	host, port, err := utils.SplitHostPort(hostport)
+	hostport, err := checkHostPort(args[0])
 	if err != nil {
 		return "", invalidHostError
 	}
-	managerHostChecker.Update(host, port, Disabled, time.Now())
+
+	managerHostChecker.Update(hostport, Disabled, time.Now())
 
 	return "+OK", nil
 }
@@ -361,7 +364,13 @@ func disconnectHandler(args []string) (string, error) {
 		return "", notEnoughArgumentsError
 	}
 
-	key := genKey(args[0], args[1])
+	user := args[0]
+	hostport, err := checkHostPort(args[1])
+	if err != nil {
+		return "", invalidHostError
+	}
+
+	key := genKey(user, hostport)
 	pc, ok := proxiedConnections[key]
 	if !ok {
 		return "+OK", fmt.Errorf("key is not present: %s", key)
@@ -376,7 +385,7 @@ func disconnectHandler(args []string) (string, error) {
 	return "+OK", nil
 }
 
-// enableHandler handles the "enable host:port" command.
+// enableHandler handles the "enable host[:port]" command.
 //
 // It returns "+OK" or an error message.
 func enableHandler(args []string) (string, error) {
@@ -384,16 +393,15 @@ func enableHandler(args []string) (string, error) {
 		return "", notEnoughArgumentsError
 	}
 
-	hostport := args[0]
-	host, port, err := utils.SplitHostPort(hostport)
+	hostport, err := checkHostPort(args[0])
 	if err != nil {
 		return "", invalidHostError
 	}
 
-	if managerHostChecker.IsDisabled(host, port) {
-		managerHostChecker.DoCheck(host, port)
+	if managerHostChecker.IsDisabled(hostport) {
+		managerHostChecker.DoCheck(hostport)
 	} else {
-		return "+OK", fmt.Errorf("host %s is already enabled", hostport)
+		log.Warning("host %s is already enabled", hostport)
 	}
 
 	return "+OK", nil
@@ -439,15 +447,14 @@ func failureHandler(args []string) (string, error) {
 		return "", notEnoughArgumentsError
 	}
 
-	hostport := args[0]
-	host, port, err := utils.SplitHostPort(hostport)
+	hostport, err := checkHostPort(args[0])
 	if err != nil {
 		return "", invalidHostError
 	}
 
 	// Check host before marking it down
-	if !managerHostChecker.IsDisabled(host, port) {
-		if managerHostChecker.DoCheck(host, port) == Up {
+	if !managerHostChecker.IsDisabled(hostport) {
+		if managerHostChecker.DoCheck(hostport) == Up {
 			return "+OK", fmt.Errorf("%s is alive", hostport)
 		}
 	} else {

sshproxy/sshproxy.go
@@ -42,34 +42,27 @@ var log = logging.MustGetLogger("sshproxy")
 
 // findDestination finds a reachable destination for the sshd server according
 // to the manager if available or the routes and route_select algorithm.
-// It returns host, port or an error if any.
-func findDestination(mclient *manager.Client, routes map[string][]string, route_select, sshd_hostport string) (string, string, error) {
+// It returns a string with host:port or an error if any.
+func findDestination(mclient *manager.Client, routes map[string][]string, route_select, sshd_hostport string) (string, error) {
 	if mclient != nil {
-		host, port, err := mclient.Connect()
+		dst, err := mclient.Connect()
 		if err != nil {
 			// disable manager in case of error
 			mclient = nil
 			log.Error("%s", err)
 		} else {
-			log.Debug("got response from manager: %s", net.JoinHostPort(host, port))
-			return host, port, nil
+			log.Debug("got response from manager: %s", dst)
+			return dst, nil
 		}
-	}
-
-	sshd_host, sshd_port, err := utils.SplitHostPort(sshd_hostport)
-	if err != nil {
-		return "", "", fmt.Errorf("invalid format for host '%s': %s", sshd_hostport, err)
 	}
 
 	checker := new(route.BasicHostChecker)
 	if destinations, present := routes[sshd_hostport]; present {
 		return route.Select(route_select, destinations, checker)
-	} else if destinations, present := routes[sshd_host]; sshd_port == utils.DefaultSshPort && present {
-		return route.Select(route_select, destinations, checker)
 	} else if destinations, present := routes[route.DefaultRouteKeyword]; present {
 		return route.Select(route_select, destinations, checker)
 	}
-	return "", "", fmt.Errorf("cannot find a route for %s and no default route configured", sshd_hostport)
+	return "", fmt.Errorf("cannot find a route for %s and no default route configured", sshd_hostport)
 }
 
 // setEnvironment sets environment variables from a map whose keys are the
@@ -241,9 +234,13 @@ func main() {
 		}()
 	}
 
-	host, port, err := findDestination(mclient, config.Routes, config.Route_Select, ssh_infos.Dst())
+	hostport, err := findDestination(mclient, config.Routes, config.Route_Select, ssh_infos.Dst())
 	if err != nil {
 		log.Fatalf("Finding destination: %s", err)
+	}
+	host, port, err := utils.SplitHostPort(hostport)
+	if err != nil {
+		log.Fatalf("Invalid destination '%s': %s", hostport, err)
 	}
 
 	// waitgroup and channel to stop our background command when exiting.
@@ -308,7 +305,7 @@ func main() {
 		recorder.Run()
 	}()
 
-	log.Notice("proxied to %s:%s", host, port)
+	log.Notice("proxied to %s", hostport)
 
 	if term.IsTerminal(os.Stdout.Fd()) {
 		err = runTtyCommand(cmd, done, recorder)

commit 1616af01391ba7b66ef79d873783d03c428366aa

route: fix default route keyword
Bug introduced in f082e38.

route/route.go
@@ -25,7 +25,7 @@ var (
 	// default algorithm to find route
 	DefaultAlgorithm = "ordered"
 	// keyword for default route
-	DefaultRouteKeyword = "default"
+	DefaultRouteKeyword = "default:22"
 )
 
 // HostChecker is the interface that wraps the Check method.