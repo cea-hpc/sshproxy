0b54ef84ec6351d1bd296983f643830f13cf05ee
Arnaud Guignard <arnaud.guignard@cea.fr>
2019-04-01 12:43:57 +0000

    misc: update sshproxyctl bash completion

--- misc/sshproxyctl-completion.bash
+++ misc/sshproxyctl-completion.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 _sshproxyctl() {
-        local cur prev opts
+        local cur prev commands opts
         COMPREPLY=()
         cur="${COMP_WORDS[COMP_CWORD]}"
         prev="${COMP_WORDS[COMP_CWORD-1]}"
@@ -13,10 +13,22 @@ _sshproxyctl() {
                 COMPREPLY=( $(compgen -W "${commands}" -- "${cur}") )
                 ;;
             show)
-                COMPREPLY=( $(compgen -W '-csv -json connections hosts' -- "${cur}") )
+                COMPREPLY=( $(compgen -W '-all -csv -json connections hosts' -- "${cur}") )
+                ;;
+            connections)
+                COMPREPLY=( $(compgen -W '-all -csv -json' -- "${cur}") )
+                ;;
+            hosts)
+                COMPREPLY=( $(compgen -W '-csv -json' -- "${cur}") )
+                ;;
+            -all)
+                COMPREPLY=( $(compgen -W '-csv -json connections' -- "${cur}") )
                 ;;
             -csv)
-                COMPREPLY=( $(compgen -W 'connections hosts' -- "${cur}") )
+                COMPREPLY=( $(compgen -W '-all connections hosts' -- "${cur}") )
+                ;;
+            -json)
+                COMPREPLY=( $(compgen -W '-all connections hosts' -- "${cur}") )
                 ;;
             -c)
                 _filedir
