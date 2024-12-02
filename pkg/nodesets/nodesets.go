d0b162027a84943bbdfcab371fd0cdaca2de1fce
Cyril Servant <cyril.servant@gmail.com>
2024-12-02 15:15:30 +0100

    indentation

--- pkg/nodesets/nodesets.go
+++ pkg/nodesets/nodesets.go
@@ -23,18 +23,18 @@ import (
 func goString(c uintptr) string {
 	// We take the address and then dereference it to trick go vet from
 	// creating a possible misuse of unsafe.Pointer
-    ptr := *(*unsafe.Pointer)(unsafe.Pointer(&c))
-    if ptr == nil {
-        return ""
-    }
-    var length int
-    for {
-        if *(*byte)(unsafe.Add(ptr, uintptr(length))) == '\x00' {
-            break
-        }
-        length++
-    }
-    return string(unsafe.Slice((*byte)(ptr), length))
+	ptr := *(*unsafe.Pointer)(unsafe.Pointer(&c))
+	if ptr == nil {
+		return ""
+	}
+	var length int
+	for {
+		if *(*byte)(unsafe.Add(ptr, uintptr(length))) == '\x00' {
+			break
+		}
+		length++
+	}
+	return string(unsafe.Slice((*byte)(ptr), length))
 }
 
 // NsChooser checks if nodeset-rs/libnodeset.so is available. If it is not
@@ -44,69 +44,68 @@ func Functions() (string, func() error, func(ns string) ([]string, error)) {
 
 	if err != nil {
 		return "libnodeset.so not found, falling back to iskylite's implementation", func() error {
-			// Close function: do nothing
-			return nil
-		}, func(ns string) ([]string, error) {
-			// Expand function
-			if strings.Contains(ns, "@") {
-				return nil, errors.New("must not contain the @ character")
+				// Close function: do nothing
+				return nil
+			}, func(ns string) ([]string, error) {
+				// Expand function
+				if strings.Contains(ns, "@") {
+					return nil, errors.New("must not contain the @ character")
+				}
+				return nodeset.Expand(ns)
 			}
-			return nodeset.Expand(ns)
-		}
 	}
 
 	return "libnodeset.so found", func() error {
-		// Close function
-		return purego.Dlclose(libnodeset)
-	}, func(ns string) ([]string, error) {
-		// Expand function
-		var ns_free_error func(uintptr)
-		purego.RegisterLibFunc(&ns_free_error, libnodeset, "ns_free_error")
-		var ns_free_nodeset func(uintptr)
-		purego.RegisterLibFunc(&ns_free_nodeset, libnodeset, "ns_free_nodeset")
-		var ns_free_iter func(uintptr)
-		purego.RegisterLibFunc(&ns_free_iter, libnodeset, "ns_free_iter")
-		var init_default_resolver func(*uintptr) int
-		purego.RegisterLibFunc(&init_default_resolver, libnodeset, "init_default_resolver")
-		var ns_parse func(string, *uintptr) uintptr
-		purego.RegisterLibFunc(&ns_parse, libnodeset, "ns_parse")
-		var ns_count func(uintptr) int
-		purego.RegisterLibFunc(&ns_count, libnodeset, "ns_count")
-		var ns_iter func(uintptr) uintptr
-		purego.RegisterLibFunc(&ns_iter, libnodeset, "ns_iter")
-		var ns_iter_next func(uintptr, *uintptr) string
-		purego.RegisterLibFunc(&ns_iter_next, libnodeset, "ns_iter_next")
-		var ns_iter_status func(uintptr) int
-		purego.RegisterLibFunc(&ns_iter_status, libnodeset, "ns_iter_status")
-		var err uintptr
-		if init_default_resolver(&err) != 0 {
-			defer ns_free_error(err)
-			return nil, errors.New(goString(err))
-		}
-		nsParsed := ns_parse(ns, &err)
-		if goString(err) != "" {
-			defer ns_free_error(err)
-			return nil, errors.New(goString(err))
-		}
-		defer ns_free_nodeset(nsParsed)
-		nsIter := ns_iter(nsParsed)
-		defer ns_free_iter(nsIter)
-		nsCount := ns_count(nsParsed)
-		hosts := make([]string, nsCount)
-		i := 0
-		for {
-			nsIterNext := ns_iter_next(nsIter, &err)
-			if nsIterNext == "" {
-				break
+			// Close function
+			return purego.Dlclose(libnodeset)
+		}, func(ns string) ([]string, error) {
+			// Expand function
+			var ns_free_error func(uintptr)
+			purego.RegisterLibFunc(&ns_free_error, libnodeset, "ns_free_error")
+			var ns_free_nodeset func(uintptr)
+			purego.RegisterLibFunc(&ns_free_nodeset, libnodeset, "ns_free_nodeset")
+			var ns_free_iter func(uintptr)
+			purego.RegisterLibFunc(&ns_free_iter, libnodeset, "ns_free_iter")
+			var init_default_resolver func(*uintptr) int
+			purego.RegisterLibFunc(&init_default_resolver, libnodeset, "init_default_resolver")
+			var ns_parse func(string, *uintptr) uintptr
+			purego.RegisterLibFunc(&ns_parse, libnodeset, "ns_parse")
+			var ns_count func(uintptr) int
+			purego.RegisterLibFunc(&ns_count, libnodeset, "ns_count")
+			var ns_iter func(uintptr) uintptr
+			purego.RegisterLibFunc(&ns_iter, libnodeset, "ns_iter")
+			var ns_iter_next func(uintptr, *uintptr) string
+			purego.RegisterLibFunc(&ns_iter_next, libnodeset, "ns_iter_next")
+			var ns_iter_status func(uintptr) int
+			purego.RegisterLibFunc(&ns_iter_status, libnodeset, "ns_iter_status")
+			var err uintptr
+			if init_default_resolver(&err) != 0 {
+				defer ns_free_error(err)
+				return nil, errors.New(goString(err))
 			}
-			hosts[i] = nsIterNext
-			i++
+			nsParsed := ns_parse(ns, &err)
+			if goString(err) != "" {
+				defer ns_free_error(err)
+				return nil, errors.New(goString(err))
+			}
+			defer ns_free_nodeset(nsParsed)
+			nsIter := ns_iter(nsParsed)
+			defer ns_free_iter(nsIter)
+			nsCount := ns_count(nsParsed)
+			hosts := make([]string, nsCount)
+			i := 0
+			for {
+				nsIterNext := ns_iter_next(nsIter, &err)
+				if nsIterNext == "" {
+					break
+				}
+				hosts[i] = nsIterNext
+				i++
+			}
+			if ns_iter_status(nsIter) != 0 {
+				defer ns_free_error(err)
+				return nil, errors.New(goString(err))
+			}
+			return hosts, nil
 		}
-		if ns_iter_status(nsIter) != 0 {
-			defer ns_free_error(err)
-			return nil, errors.New(goString(err))
-		}
-		return hosts, nil
-	}
 }
-
